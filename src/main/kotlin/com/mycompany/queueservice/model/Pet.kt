918dccf924c52525974189624d4d3d31ab76f081
jaqueliniRocha <jaquelini.rocha@hotmail.com>
2021-11-07 21:42:29 -0300

    adicao de pets

---
+++ src/main/kotlin/com/mycompany/queueservice/model/Pet.kt
@@ -0,0 +1,25 @@
+package com.mycompany.queueservice.model
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.validation.constraints.NotBlank
+
+@Entity
+data class Pet (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long,
+
+    @field:NotBlank
+    val name: String,
+
+    @field:NotBlank
+    var breed: String
+
+ ) {
+
+    override fun toString(): String {
+        return "Pet(id=$id, name='$name', breed='$breed')"
+    }
+}
--- src/main/kotlin/com/mycompany/queueservice/model/User.kt
+++ src/main/kotlin/com/mycompany/queueservice/model/User.kt
@@ -1,6 +1,9 @@
 package com.mycompany.queueservice.model
 
+import org.hibernate.annotations.Cascade
+import org.hibernate.annotations.CascadeType
 import javax.persistence.*
+import javax.validation.Valid
 import javax.validation.constraints.Email
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
@@ -18,9 +21,17 @@ data class User(
 
     @Enumerated(EnumType.STRING)
     val profile: UserProfile?,
+
+    @field:NotNull
+    @field:Valid
+    @Cascade(CascadeType.ALL)
+    @OneToMany
+    @JoinColumn(name = "user_id")
+    val pets: Collection<Pet>,
 ) {
 
     override fun toString(): String {
-        return "User(id=$id, firstName='$firstName', lastName='$lastName', email='$email', category=$category)"
+        return "User(id=$id, firstName=$firstName, lastName=$lastName, email=$email, password=$password, " +
+                "category=$category, profile=$profile, pets=$pets)"
     }
 }
